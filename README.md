3b8ca1e2b02c53dc71d580f1a0fac473b48d07f6
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2021-02-11 15:43:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# svelte-book-preview-app
+
+「【Svelte + TypeScript + tailwindcss】本検索サイト チュートリアル」の写経
+https://zenn.dev/azukiazusa/books/9dac1257c77f47
---
+++ src/pages/SearchBook.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  import InfiniteScroll from 'svelte-infinite-scroll'
+  import SearchBar from '../components/SearchBar.svelte'
+  import BookCard from '../components/BookCard.svelte'
+  import Spinner from '../components/Spinner.svelte'
+  import { books } from '../store/book'
+  import RepositoryFactory, { BOOK } from '../repositories/RepositoryFactory'
+  const BookRepository = RepositoryFactory[BOOK]
+
+  let q = ''
+  let startIndex = 0
+  let empty = false
+  let promise: Promise<void>
+
+  let totalItems = 0
+  $: hasMore = totalItems > $books.length
+
+  const handleSubmit = () => {
+    if (!q.trim()) return
+    promise = getBooks()
+  }
+
+  const getBooks = async () => {
+    books.reset()
+    empty = false
+    startIndex = 0
+    const result = await BookRepository.get({ q })
+    empty = result.totalItems === 0
+    totalItems = result.totalItems
+    books.add(result.items)
+  }
+
+  const handleLoadMore = () => {
+    startIndex += 10
+    promise = getNextPage()
+  }
+
+  const getNextPage = async () => {
+    const result = await BookRepository.get({ q, startIndex })
+
+    const booksIds = $books.map((book) => book.id)
+    const filteredItems = result.items.filter((item) => {
+      return !booksIds.includes(item.id)
+    })
+    books.add(filteredItems)
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <SearchBar bind:value={q} />
+</form>
+
+<div class="text-center mt-4">
+  {#if empty}
+    <div>検索結果が見つかりませんでした</div>
+  {:else}
+    <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+      {#each $books as book (book.id)}
+        <BookCard {book} />
+      {/each}
+    </div>
+  {/if}
+  <InfiniteScroll window threshold={100} on:loadMore={handleLoadMore} {hasMore} />
+  {#await promise}
+    <div class="flex justify-center">
+      <Spinner />
+    </div>
+  {:catch e}
+    <span class="text-red-600 text-sm">
+      {e.message}
+    </span>
+  {/await}
+</div>
